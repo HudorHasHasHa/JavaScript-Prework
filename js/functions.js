26f13dce49dcb0df4cb6e16d7952c7ded2dfce1a
Igor Jankowski <jankowskiigor5@gmail.com>
2020-08-15 09:25:51 +0200

    paper

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,6 @@
 function printMessage(msg){
-	let div = document.createElement('div');
+    let div = document.createElement('div');
+    let playerMove = 'papier';
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
